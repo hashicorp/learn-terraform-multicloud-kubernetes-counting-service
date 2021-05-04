86a5958f5fc8ab136c8fa3d1424a35de4bf0d425
ritsok <rita@hashicorp.com>
2021-05-04 15:25:42 -0400

    update provider config, readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Learn Terraform - Deploy Federated Multi-Cloud Kubernetes Clusters
+
+This is a companion repository for the [Deploy Federated Multi-Cloud Kubernetes
+Clusters
+guide](https://learn.hashicorp.com/tutorials/terraform/multicloud-kubernetes)
+on [HashiCorp Learn](https://learn.hashicorp.com/). It contains Terraform
+configuration files to deploy a sample application for verifying Consul
+federation across Kubernetes clusters.
--- main.tf
+++ main.tf
@@ -7,12 +7,21 @@ data "terraform_remote_state" "aks" {
   }
 }
 
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_kubernetes_cluster" "cluster" {
+  name                = data.terraform_remote_state.aks.outputs.kubernetes_cluster_name
+  resource_group_name = data.terraform_remote_state.aks.outputs.resource_group_name
+}
+
 provider "kubernetes" {
   alias                  = "aks"
-  host                   = data.terraform_remote_state.aks.outputs.host
-  client_certificate     = base64decode(data.terraform_remote_state.aks.outputs.client_certificate)
-  client_key             = base64decode(data.terraform_remote_state.aks.outputs.client_key)
-  cluster_ca_certificate = base64decode(data.terraform_remote_state.aks.outputs.cluster_ca_certificate)
+  host                   = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
 }
 
 resource "kubernetes_service_account" "counting" {
@@ -55,14 +64,21 @@ data "terraform_remote_state" "eks" {
   }
 }
 
+provider "aws" {
+  region = data.terraform_remote_state.eks.outputs.region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
 
 provider "kubernetes" {
   alias                  = "eks"
-  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
-  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority)
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
-    args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.eks.outputs.cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
     command     = "aws"
   }
 }
